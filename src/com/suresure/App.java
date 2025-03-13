dc756af9aa9f3955b58340f97cc9154bd5acd716
SUSUshuo <10819724+susushuo@user.noreply.gitee.com>
2025-03-13 22:28:45 +0800

    feature-登陆功能

--- src/com/suresure/App.java
+++ src/com/suresure/App.java
@@ -1,9 +1,10 @@
 package com.suresure;
 
 import com.suresure.ui.GameJFrame;
+import com.suresure.ui.LoginJFrame;
 
 public class App {
     public static void main(String[] args) {
-        new GameJFrame();
+        new LoginJFrame();
     }
 }
